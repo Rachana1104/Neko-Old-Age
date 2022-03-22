58b6f6bfb3a26b623769eb44521bc17ff326da6e
Sanika <sanikaardekar@gmail.com>
2022-03-22 21:19:20 +0530

    login signup for relative and admin done

--- src/App.js
+++ src/App.js
@@ -1,10 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import LoginPage from "./pages/LoginSignup/Admin/LoginPage";
+import SignupPage from "./pages/LoginSignup/Admin/SignupPage";
+import LoginPage1 from "./pages/LoginSignup/Relative/LoginPage";
+import SignupPage1 from "./pages/LoginSignup/Relative/SignupPage";
+import Aos from "aos";
+import "aos/dist/aos.css";
+import {useEffect} from 'react';
 
 function App() {
+
+  useEffect(() => {
+    Aos.init({ duration: 1000, once: true });
+  }, []);
+
   return (
     <div className="App">
-      hello rachana hehehehhehehe
+      <Router>
+      <Routes>
+
+      <Route exact path="/admin/login" element={<LoginPage />} />
+      <Route exact path="/admin/signup" element={<SignupPage />} />
+      <Route exact path="/relative/signup" element={<SignupPage1 />} />
+      <Route exact path="/relative/login" element={<LoginPage1 />} />
+      </Routes>
+      
+      </Router>
     </div>
   );
 }
